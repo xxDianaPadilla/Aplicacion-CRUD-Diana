2e157de984789f75709c4c8f050aedfcd3d22118
Diana <20200005@ricaldone.edu.sv>
2024-06-05 18:07:51 -0600

    Finalización de diseño

--- app/src/main/java/diana/padilla/aplicacioncruddiana/activity_tickets_form.kt
+++ app/src/main/java/diana/padilla/aplicacioncruddiana/activity_tickets_form.kt
@@ -1,29 +1,20 @@
 package diana.padilla.aplicacioncruddiana
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class activity_tickets_form : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tickets_form)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnRegister = findViewById<Button>(R.id.btnRegister)
-
-        btnRegister.setOnClickListener {
-            val registerScreen = Intent(this, activity_register::class.java)
-            startActivity(registerScreen)
         }
     }
 }
