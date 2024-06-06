f1b4486192930e4c6ee0b2e42f02eb816bea9101
Diana <20200005@ricaldone.edu.sv>
2024-06-05 18:18:42 -0600

    Agregue unas cosas de diseño extra

--- app/src/main/java/diana/padilla/aplicacioncruddiana/activity_tickets_form.kt
+++ app/src/main/java/diana/padilla/aplicacioncruddiana/activity_tickets_form.kt
@@ -1,6 +1,8 @@
 package diana.padilla.aplicacioncruddiana
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class activity_tickets_form : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnViewTickets = findViewById<Button>(R.id.btnViewTickets)
+
+        btnViewTickets.setOnClickListener {
+            val myTicketsScreen = Intent(this, activity_my_tickets::class.java)
+            startActivity(myTicketsScreen)
         }
     }
 }
