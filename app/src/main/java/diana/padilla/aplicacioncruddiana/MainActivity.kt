5c2683004701a79552bfd9358cb0ec28a4922008
Diana <20200005@ricaldone.edu.sv>
2024-06-06 20:43:42 -0600

    Finalización de Login

--- app/src/main/java/diana/padilla/aplicacioncruddiana/MainActivity.kt
+++ app/src/main/java/diana/padilla/aplicacioncruddiana/MainActivity.kt
@@ -3,10 +3,16 @@ package diana.padilla.aplicacioncruddiana
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +30,37 @@ class MainActivity : AppCompatActivity() {
         btnRegister.setOnClickListener {
             val registerScreen = Intent(this, activity_register::class.java)
             startActivity(registerScreen)
+        }
+
+        val txtEmail = findViewById<EditText>(R.id.txtEmail)
+        val txtPassword = findViewById<EditText>(R.id.txtPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val ticketsFormScreen = Intent(this, activity_tickets_form::class.java)
+
+            GlobalScope.launch(Dispatchers.IO){
+                val objConexion = ClaseConexion().cadenaConexion()
+
+                val checkUser = objConexion?.prepareStatement("select * from Users where email = ? and password = ?")!!
+                checkUser.setString(1, txtEmail.text.toString())
+                checkUser.setString(2, txtPassword.text.toString())
+                val result = checkUser.executeQuery()
+
+                if(txtEmail.text.isEmpty() || txtPassword.text.isEmpty()){
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Please fill out all the data 😮", Toast.LENGTH_LONG).show()
+                    }
+                }else if(result.next()){
+                    startActivity(ticketsFormScreen)
+                }else{
+                    runOnUiThread {
+                        txtEmail.text.clear()
+                        txtPassword.text.clear()
+                        Toast.makeText(this@MainActivity, "User not found 😪, try again", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
         }
     }
 }
