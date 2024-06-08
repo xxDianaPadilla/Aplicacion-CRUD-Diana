c902c7da31291d0b1638b01dcc1f61290c0006f6
Diana <20200005@ricaldone.edu.sv>
2024-06-07 20:02:36 -0600

    Finalización de pantalla de envío de tickets

--- app/src/main/java/diana/padilla/aplicacioncruddiana/activity_tickets_form.kt
+++ app/src/main/java/diana/padilla/aplicacioncruddiana/activity_tickets_form.kt
@@ -3,10 +3,16 @@ package diana.padilla.aplicacioncruddiana
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 
 class activity_tickets_form : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +30,54 @@ class activity_tickets_form : AppCompatActivity() {
         btnViewTickets.setOnClickListener {
             val myTicketsScreen = Intent(this, activity_my_tickets::class.java)
             startActivity(myTicketsScreen)
+        }
+
+        val txtTicketNumber = findViewById<EditText>(R.id.txtTicketNumber)
+        val txtTitleTicket = findViewById<EditText>(R.id.txtTitleTicket)
+        val txtTicketDescription = findViewById<EditText>(R.id.txtTicketDescription)
+        val txtAuthorTicket = findViewById<EditText>(R.id.txtAuthorTicket)
+        val txtEmailAuthor = findViewById<EditText>(R.id.txtEmailAuthor)
+        val txtCreationDate = findViewById<EditText>(R.id.txtCreationDate)
+        val txtStatus = findViewById<EditText>(R.id.txtStatus)
+        val txtTicketEndDate = findViewById<EditText>(R.id.txtTicketEndDate)
+        val btnSendTicket = findViewById<Button>(R.id.btnSendTicket)
+
+        btnSendTicket.setOnClickListener {
+
+            if(txtTicketNumber.text.isEmpty() || txtTitleTicket.text.isEmpty() || txtTicketDescription.text.isEmpty() || txtAuthorTicket.text.isEmpty() || txtAuthorTicket.text.isEmpty() || txtEmailAuthor.text.isEmpty() || txtCreationDate.text.isEmpty() || txtStatus.text.isEmpty() || txtTicketEndDate.text.isEmpty()){
+                runOnUiThread {
+                    Toast.makeText(this, "Please fill out all the data 😮", Toast.LENGTH_LONG).show()
+                }
+            }else{
+                CoroutineScope(Dispatchers.IO).launch {
+                    val objConexion = ClaseConexion().cadenaConexion()
+
+                    val addTicket = objConexion?.prepareStatement("insert into Tickets (ticket_number, ticket_title, ticket_description, ticket_author, contact_email, creation_date, ticket_status, completion_date) values (?, ?, ?, ?, ?, ?, ?, ?)")!!
+
+                    addTicket.setString(1, txtTicketNumber.text.toString())
+                    addTicket.setString(2, txtTitleTicket.text.toString())
+                    addTicket.setString(3, txtTicketDescription.text.toString())
+                    addTicket.setString(4, txtAuthorTicket.text.toString())
+                    addTicket.setString(5, txtEmailAuthor.text.toString())
+                    addTicket.setString(6, txtCreationDate.text.toString())
+                    addTicket.setString(7, txtStatus.text.toString())
+                    addTicket.setString(8, txtTicketEndDate.text.toString())
+                    addTicket.executeQuery()
+
+                    runOnUiThread {
+                        txtTicketNumber.text.clear()
+                        txtTitleTicket.text.clear()
+                        txtTicketDescription.text.clear()
+                        txtAuthorTicket.text.clear()
+                        txtEmailAuthor.text.clear()
+                        txtCreationDate.text.clear()
+                        txtStatus.text.clear()
+                        txtTicketEndDate.text.clear()
+                        Toast.makeText(this@activity_tickets_form, "Ticket sent successfully 🎉", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
         }
     }
 }
--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -7,7 +7,7 @@ class ClaseConexion {
     fun cadenaConexion(): Connection?{
 
         try {
-            val url = "jdbc:oracle:thin:@192.168.0.3:1521:xe"
+            val url = "jdbc:oracle:thin:@192.168.0.4:1521:xe"
             val usuario = "DIANA_DEVELOPER"
             val contrasena = "Diana#2006"
 
